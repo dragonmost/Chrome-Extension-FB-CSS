e602a31272c0225c2a03da41a870b3ae56fded7e
dragonmost <dragonmost@live.ca>
2015-05-13 21:29:02 -0400

    Code

---
+++ FB/fb adb.js
@@ -0,0 +1,23 @@
+function remove()
+{
+	try
+	{
+		document.getElementById('pagelet_ego_pane_w').innerHTML = '';
+	}
+	
+	try
+	{
+		document.getElementById('pagelet_ego_pane').innerHTML = '';
+	}
+	
+	try
+	{
+		document.getElementById('pagelet_side_ads').innerHTML = '';
+	}
+	
+	try
+	{
+		document.getElementById('fbPhotoSnowliftAdsSide').innerHTML = '';
+	}
+	
+}
